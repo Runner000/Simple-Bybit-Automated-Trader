0ee2b6948ea287c9464cc8e9840f031643c5136f
Runner <118213354+Runner000@users.noreply.github.com>
2025-03-09 11:16:16 -0700

    Delete src directory

--- src/utils/misc.py
+++
@@ -1,7 +0,0 @@
-from time import time_ns, strftime
-
-def datetime_now() -> str:
-    return strftime("%Y-%m-%d %H:%M:%S") + f".{(time_ns()//1000) % 1000000:05d}"
-
-def time_ms() -> int:
-    return time_ns()//1_000_000
